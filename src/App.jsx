ded910e4a8f1d7a62cc698690519aad6de11c6d1
omnikorn <harith@Hariths-MacBook-Air-2.local>
2023-07-27 14:01:04 +0100

    toggle form on and off

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useState } from "react"
 import AddTask from "./components/AddTask";
 
 function App() {
-  
+  const [showAddTask, setShowAddTask]= useState(false)
   const [tasks, setTasks] = useState([{
     id: 1,
     text: "Doctors app",
@@ -44,8 +44,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={()=>setShowAddTask(!showAddTask)}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ("No Tasks to Show")}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 import Button from "./Button"
 
-const Header = () => {
+const Header = ({onAdd}) => {
 
-const onClick =(e)=>{
-    console.log(e)
-}
+
   return (
     <header className="header">
         <h1>TASK TRACKER</h1>
-        <Button color="green" text="ADD" onClick={onClick}/>
+        <Button color="green" text="ADD" onClick={onAdd}/>
     </header>
   )
 }
