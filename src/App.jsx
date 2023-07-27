f199b1f897d540f6509657e893ab4b2faf8df2bb
omnikorn <harith@Hariths-MacBook-Air-2.local>
2023-07-27 12:12:21 +0100

    added ading tasks functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react"
+import AddTask from "./components/AddTask";
 
 function App() {
   
@@ -37,6 +38,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask />
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ("No Tasks to Show")}
     </div>
   );
