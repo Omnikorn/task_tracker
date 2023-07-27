1ee919c22bd8d03d3d349a704d9d6ef82f97c8ee
omnikorn <harith@Hariths-MacBook-Air-2.local>
2023-07-27 13:53:03 +0100

    finished addtask functionality

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,13 @@ function App() {
       day: "Saturday 10th at 9am",
       reminder: false
     }])
+    // adding a task
+    const addTask = (task) => {
+      const id= Math.floor(Math.random()*10000)+1
+      const newTask = {id, ...task}
+      setTasks([...tasks, newTask])
+    } 
+
 
     //delete task
     const deleteTask = (id) =>{
@@ -38,7 +45,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ("No Tasks to Show")}
     </div>
   );
