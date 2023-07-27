dddded7168d62f8f60055db508b2e55a2152d8fb
omnikorn <harith@Hariths-MacBook-Air-2.local>
2023-07-27 11:07:47 +0100

    building the delete function for tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react"
 
 function App() {
   
+  const [tasks, setTasks] = useState([{
+    id: 1,
+    text: "Doctors app",
+    day: "wednesday 7th at 2pm",
+    reminder: true
+  },
+    {
+      id: 2,
+      text: "kikis birthday",
+      day: "friday  9th at 6pm",
+      reminder: true
+    },
+    {
+      id: 3,
+      text: "hair cut",
+      day: "Saturday 10th at 9am",
+      reminder: false
+    }])
+
+    //delete task
+    const deleteTask = (id) =>{
+      console.log("delete", id)
+    }
+
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,18 @@
-const tasks = [{
-    id: 1,
-    text: "Doctors app",
-    day: "wednesday 7th at 2pm",
-    reminder: true
-},
-{
-    id: 2,
-    text: "kikis birthday",
-    day: "friday  9th at 6pm",
-    reminder: true
-},
-{
-    id: 3,
-    text: "hair cut",
-    day: "Saturday 10th at 9am",
-    reminder: false
-}]
+import Task from "./Task"
 
-const Tasks = () => {
+
+
+
+
+
+const Tasks = ({tasks}) => {
+    
+
+
     return (
         <>
             {tasks.map((task) => {
-                return (<h3>{task.text}</h3>)
+                return (<Task key={task.id} task={task} />)
             })}
         </>
     )
